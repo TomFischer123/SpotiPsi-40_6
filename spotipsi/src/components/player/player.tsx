10480667a479f868cd8063ad34774e6cc404d5a3
tomfischer123 <etomfischer@gmail.com>
2026-03-18 17:22:33 +0200

    music player

--- spotipsi/src/components/player/player.tsx
+++ spotipsi/src/components/player/player.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useStyles from "./stylesPlayer";
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
@@ -8,6 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
+import { useRef } from 'react';
 
 interface Song {
     id: string,
@@ -15,49 +16,110 @@ interface Song {
     artist: string,
     album: string,
 }
-interface Props{
-    currentSong:Song | undefined,
-    isPlaying:boolean,
-    queue:Song[],
-    currentTime:string,
-    duration:string
+interface Props {
+    currentSong: Song | undefined,
+    isPlaying: boolean,
+    queue: Song[],
+    currentTime: string,
+    duration: string
 }
 
-const Player: React.FC<Props> = ({currentSong,isPlaying,queue,currentTime,duration}:Props) => {
+const Player: React.FC<Props> = ({ currentSong, isPlaying, queue }: Props) => {
     const { classes } = useStyles();
-    const [click,setClick]  =useState(0)
-
-    const handleClickPlay =() =>{
-        if(click ===0)
-        {
-            setClick(1)
-            isPlaying = true
+    const [isplaying, setIsPlaiyng] = useState(isPlaying)
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [trackProgress, setTrackProgress] = useState(0);
+    const audioRef = useRef(new Audio(currentSong ? `/songs/${currentSong.id}.mp3` : ""));
+    useEffect(() => {
+        if (currentSong) {
+            audioRef.current = new Audio(`/songs/${currentSong.id}.mp3`);
         }
-        else{
-            setClick(0)
-            isPlaying = false
+    }, [currentSong]);
+
+    const intervalRef = useRef(undefined);
+    const isReady = useRef(false);
+
+    const duration = audioRef.current.duration;
+
+    const toPrevTrack = () => {
+        console.log(trackIndex)
+        if (trackIndex - 1 < 0) {
+            audioRef.current.pause();
+            setIsPlaiyng(false)
+            setTrackIndex(queue.length - 1);
+
+        } else {
+            audioRef.current.pause();
+            setIsPlaiyng(false)
+            setTrackIndex(prev => prev - 1);
+            console.log(trackIndex)
         }
     }
 
+    const toNextTrack = () => {
+        if (trackIndex < queue.length - 1) {
+            audioRef.current.pause();
+            setIsPlaiyng(false)
+            setTrackIndex(trackIndex + 1);
+        } else {
+            audioRef.current.pause();
+            setIsPlaiyng(false)
+            setTrackIndex(0);
+        }
+    }
+
+    useEffect(() => {
+        // Pause and clean up on unmount
+        return () => {
+            audioRef.current.pause();
+            clearInterval(intervalRef.current);
+        }
+    }, []);
+
+    useEffect(() => {
+        audioRef.current.pause();
+        if (!queue.length) return;
+        const song = queue[trackIndex];
+        console.log(song)
+        audioRef.current = new Audio(`/songs/${song.id}.mp3`);
+    }, [trackIndex, queue]);
+
+
+    const handleClickPlay = () => {
+        if (isplaying === false) {
+            console.log("playing")
+            setIsPlaiyng(true)
+        }
+        else {
+            setIsPlaiyng(false)
+        }
+    }
+
+    useEffect(() => {
+        if (isplaying) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isplaying]);
+
     return (
         <div className={classes.footer}>
-            <Typography className={classes.song}><b>{currentSong === undefined? "un":currentSong.name}</b></Typography>
-            <Typography className={classes.artist}>{currentSong === undefined? "un":currentSong.artist}</Typography>
+            <Typography className={classes.song}><b>{queue[trackIndex] === undefined ? "un" : queue[trackIndex].name}</b></Typography>
+            <Typography className={classes.artist}>{queue[trackIndex] === undefined ? "un" : queue[trackIndex].artist}</Typography>
             <div className={classes.buttons}>
-                <IconButton className={classes.play} ><SkipPreviousIcon fontSize="small" /></IconButton>
-                <IconButton className={classes.play} onClick={handleClickPlay}>{ click===0? <PlayArrowIcon />:<PauseIcon/>}</IconButton>
-                <IconButton className={classes.play} ><SkipNextIcon fontSize="small" /></IconButton>
+                <IconButton className={classes.play} onClick={toPrevTrack}><SkipPreviousIcon fontSize="small" /></IconButton>
+                <IconButton className={classes.play} onClick={handleClickPlay}>{isplaying === false ? <PlayArrowIcon /> : <PauseIcon />}</IconButton>
+                <IconButton className={classes.play} onClick={toNextTrack}><SkipNextIcon fontSize="small" /></IconButton>
             </div>
-            <Box >
+            <Box>
                 <Slider className={classes.slider}
                     size="small"
                     defaultValue={0}
                     aria-label="Small"
                     valueLabelDisplay="auto"
-
                 />
             </Box>
-
         </div>
     );
 }
